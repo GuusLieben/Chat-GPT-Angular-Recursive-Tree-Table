2b16c0a85dd6c9273666a550a221c2b0f9b30938
Guus Lieben <guus@xendox.com>
2023-01-06 19:54:55 +0100

    Implemented a recursive tree table with expand/collapse buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import words from "random-words";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ClarityDemo';
+
+    columns: string[] = [
+        'level',
+        'description',
+        'keywords',
+        'randomNumber',
+        'randomBoolean',
+    ]
+    treeData: any[];
+    selectedNode: any;
+
+    constructor() {
+        this.treeData = this.generateRandomTreeData(50);
+        this.addParentAndLevelProperties(this.treeData);
+    }
+
+    generateRandomTreeData(numElements: number): any[] {
+        const treeData: any[] = [];
+
+        for (let i = 0; i < numElements; i++) {
+            treeData.push({
+                name: 'Node ' + (i + 1),
+                children: this.generateRandomChildren(i + 1, 0),
+            });
+        }
+
+        return treeData;
+    }
+
+    generateRandomChildren(nodeNumber: number, level: number): any[] {
+        const numChildren = Math.floor(Math.random() * 3);
+
+        const children: any[] = [];
+
+        for (let i = 0; i < numChildren; i++) {
+            children.push({
+                name: 'Child ' + nodeNumber + '-' + (i + 1),
+                children: level < 4 ? this.generateRandomChildren(i + 1, level + 1) : [],
+            });
+        }
+
+        return children;
+    }
+
+    addParentAndLevelProperties(treeData: any[], parent: any = null, level: number = 0) {
+        treeData.forEach(node => {
+            node.parent = parent;
+            node.level = level;
+            node.description = node.name + ' (Level ' + level + ')';
+            node.keywords = words({ exactly: 3, join: ', ' });
+            node.randomNumber = Math.floor(Math.random() * 100);
+            node.randomBoolean = Math.random() < 0.5;
+
+            node.expanded = true;
+
+            if (node.children) {
+                this.addParentAndLevelProperties(node.children, node, level + 1);
+            }
+        });
+    }
+
+    rowSelected(node: any) {
+        if (!(node.children && node.children.length > 0)) {
+            console.log('Selected node: ', node);
+            this.selectedNode = node;
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
